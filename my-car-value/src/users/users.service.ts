f506689e7a1a254d3b65d51c469ca24dec18599a
begalievn <begaliev.info@gmail.com>
2024-01-25 00:39:19 +0600

    Add my-car-value

---
+++ my-car-value/src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.userRepo.create({ email, password });
+    console.log('user', user);
+    return this.userRepo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.userRepo.findOneById(id);
+  }
+
+  find(email: string) {
+    return this.userRepo.find({ where: { email } });
+  }
+
+  async update(id: number, attr: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attr);
+
+    return this.userRepo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.userRepo.remove(user);
+  }
+}
